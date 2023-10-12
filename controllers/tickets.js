0760552822fde527c9ec23a0dd42a84da1230c84
gryantgilgan <bryantgilgan@gmail.com>
2023-10-12 16:04:09 -0700

    updates controller and new for tickets

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -7,18 +7,27 @@ module.exports = {
 }
 
 async function addTicket(req, res) {
-    // find flight by id and assign to variable called flight
-    const flight = await Flight.findById(req.params.id);
-    // render new ticket template
-    // pass the flight
-    res.render('tickets/new', { flight });
+    try {
+        // find flight by id and assign to variable called flight
+        const flight = await Flight.findById(req.params.id);
+        // render new ticket template
+        // pass the flight
+        res.render('tickets/new', { flight });
+    } catch(err) {
+        console.log(err);
+    }
 }
 
 async function create(req, res) {
+    const ticketInfo = req.body;
+    const flightId = req.params.id;
+    console.log('before', ticketInfo);
+    ticketInfo.flight = flilghtId;
+    console.log('after', ticketInfo);
     req.body.flight = req.params.id;
     try {
-        await Ticket.create(req.body)
-        res.redirect(`/flights/${req.params.id}`)
+        await Ticket.create(ticketInfo)
+        res.redirect(`/flights/${flightId}`)
     } catch(err) {
         console.log(err);
     }
--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use('/flights', flightsRouter);
 app.use('/', destinationsRouter);
 app.use('/', ticketsRouter);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
